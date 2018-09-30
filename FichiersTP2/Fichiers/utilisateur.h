de572f08094625a864e21c02a7bf1208634d897a
Raph <raph5278@hotmail.com>
2018-09-30 12:52:39 -0400

    Correction du nom de variable

--- FichiersTP2/Fichiers/utilisateur.h
+++ FichiersTP2/Fichiers/utilisateur.h
@@ -34,7 +34,7 @@ public:
 	void setNom(const string& nom);
 
 	// Surcharge de l'operateur +=. Permet d’ajouter une dépense à l’utilisateur. Renvoie ensuite une référence à l’utilisateur.
-	Utilisateur& operator+=(Depense* depense);
+	Utilisateur& operator+=(Depense* pointeurDepense);
 
 	// Surcharge de l'operateur =. Écrase les attributs de l’utilisateur par les attributs
 	// de l’utilisateur passé en paramètre et qui renvoie ensuite une référence à l’utilisateur.
